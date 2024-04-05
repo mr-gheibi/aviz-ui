c473a2e5654b886d9bc7d257007361aec3bb0bbc
Amirmohammad Gheibi <amirmohammag@gmail.com>
2024-04-05 20:01:00 +0330

    Fix SmoothPageIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,56 @@ class App extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final controller = PageController(viewportFraction: 0.8, keepPage: true);
+
+  final pages = List.generate(
+    3,
+    (index) => Column(
+      children: [
+        SizedBox(height: 56),
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Image(image: AssetImage('assets/images/mainpage_lines.png')),
+            Image(image: AssetImage('assets/images/mainpage_pic.png')),
+          ],
+        ),
+        SizedBox(height: 32),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          textDirection: TextDirection.rtl,
+          children: [
+            Text(
+              " اینجا محل",
+              style: mainTheme().textTheme.titleLarge,
+            ),
+            Image(image: AssetImage('assets/images/logo.png')),
+            Text(
+              "آگهی شماست ",
+              style: mainTheme().textTheme.titleLarge,
+            )
+          ],
+        ),
+        SizedBox(height: 16),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 35),
+          child: Text(
+            'در آویز ملک خود را برای فروش،اجاره و رهن آگهی کنید و یا اگر دنبال ملک با مشخصات دلخواه خود هستید آویز ها را ببینید',
+            style: mainTheme().textTheme.titleSmall,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -34,42 +82,16 @@ class MainScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 56),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Image(image: AssetImage('assets/images/mainpage_lines.png')),
-                Image(image: AssetImage('assets/images/mainpage_pic.png')),
-              ],
-            ),
-            SizedBox(height: 32),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              textDirection: TextDirection.rtl,
-              children: [
-                Text(
-                  " اینجا محل",
-                  style: mainTheme().textTheme.titleLarge,
-                ),
-                Image(image: AssetImage('assets/images/logo.png')),
-                Text(
-                  "آگهی شماست ",
-                  style: mainTheme().textTheme.titleLarge,
-                )
-              ],
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 35),
-              child: Text(
-                'در آویز ملک خود را برای فروش،اجاره و رهن آگهی کنید و یا اگر دنبال ملک با مشخصات دلخواه خود هستید آویز ها را ببینید',
-                style: mainTheme().textTheme.titleSmall,
-                textAlign: TextAlign.center,
+            Expanded(
+              child: PageView.builder(
+                controller: controller,
+                itemBuilder: (context, index) {
+                  return pages[index % pages.length];
+                },
               ),
             ),
-            Spacer(),
-            AnimatedSmoothIndicator(
-              activeIndex: 0,
+            SmoothPageIndicator(
+              controller: controller,
               count: 3,
               effect: ExpandingDotsEffect(
                 activeDotColor: primaryColor,
